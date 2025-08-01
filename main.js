2671883814e0b55bc99db92d55df9b4eef8833a1
hp <hp@DESKTOP-6LPSDT1>
2025-08-01 09:29:08 +0400

    Add final download page

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
-    icon: path.join(__dirname, 'assets/pixiwall_icon.ico')
   });
-  win.loadURL('http://localhost:51234');
-}
 
-function startServer() {
-  const serverApp = express();
-  serverApp.use(cors());
-  serverApp.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-  serverApp.use(express.static(path.join(__dirname, 'frontend')));
-  serverApp.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/index.html'));
-  });
-  serverApp.listen(51234, () => {
-    console.log("Server running on http://localhost:51234");
-  });
+  win.loadFile("public/download/index.html");
 }
 
 app.whenReady().then(() => {
-  startServer();
   createWindow();
 });
